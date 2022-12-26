f5933308e8f8f580e87b57c44af32355c6cf7cfa
VlV <VlV@VlV.com>
2022-12-25 23:04:09 -0600

    Feat->Inicio y Crea AddCategory

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
-function App() {
+import { useState } from 'react';
+import { AddCategory } from './components/AddCategory/AddCategory';
+
+export default function App() {
+  const [categories, setCategories] = useState(['React', 'Vue', 'Angular']);
+
+  const onAddCategory = () => {
+    setCategories([...categories, 'Holis']);
+  };
+
   return (
     <>
       <h1>Hello React!</h1>
+      <AddCategory />
+      <button onClick={onAddCategory}>Agregar</button>
+      <ol>
+        {categories.map((category) => (
+          <li key={category}>{category}</li>
+        ))}
+      </ol>
     </>
   );
 }
-
-export default App;
